05dff9aac6b9fa57b28e0b1cc45cfb16cda2f0a2
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-06-29 10:31:55 -0500

    Created mr_ash_demo.R demo script.

---
+++ inst/code/mr_ash_demo.R
@@ -0,0 +1,13 @@
+set.seed(1)
+n <- 400
+p <- 1000
+X <- matrix(rnorm(n*p),n,p)
+b <- double(p)
+b[1:10] <- rnorm(10)
+y <- drop(X %*% b + rnorm(n))
+fit <- mr.ash(X,y,max.iter = 500,tol = list(convtol = 1e-12))
+elbo <- fit$varobj
+elbo <- elbo - min(elbo) + 1e-6
+plot(1:fit$iter,elbo,type = "l",log = "y",lwd = 2,col = "dodgerblue")
+plot(b,fit$beta,pch = 20,col = "darkblue",xlab = "true",ylab = "estimate")
+abline(a = 0,b = 1,col = "skyblue",lty = "dotted")
