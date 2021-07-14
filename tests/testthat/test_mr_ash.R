289665f50953dad3f9bb70b72d482f89897c445b
lwa19 <lwang19@uchicago.edu>
2021-07-14 14:19:12 -0500

    added fit check; accomodate mr.ash interface updates

--- tests/testthat/test_mr_ash.R
+++ tests/testthat/test_mr_ash.R
@@ -26,29 +26,8 @@ test_that("re-running mr.ash after solution has converged yields same fit",{
   expect_equal(fit1,fit2,scale = 1,tolerance = 1e-8)
 })
 
-# Test that mr.ash outputs the same phi values as mr.ash.alpha
-test_that("equal phi values", {
-  # Simulate X and y
-  set.seed(1)
-  n     <- 200
-  p     <- 400
-  pve   <- 0.2
-  s     <- 10
-  data  <- simulate_data(n, p, pve, s)
-  
-  # fitting mr.ash.alpha
-  capture.output(fit.alpha <- mr.ash.alpha::mr.ash(data$X, data$y))
-  phi.alpha <- mr.ash.alpha::get.full.posterior(fit.alpha)$phi
-  
-  # fitting mr.ash
-  capture.output(fit <- mr_ash(data$X, data$y))
-  
-  # Check same phi value
-  expect_equal(fit$phi, phi.alpha, scale = 1,tolerance = 1e-8)
-})
-
 # We want to test that mr.ash outputs the same result as mr.ash.alpha
-test_that("agreement with mr.ash.alpha", {
+test_that("check identical fit", {
   
   # Simulate X and y
   set.seed(1)
@@ -63,7 +42,9 @@ test_that("agreement with mr.ash.alpha", {
   fit.alpha$beta   <- drop(fit.alpha$beta)
   fit.alpha$pi     <- drop(fit.alpha$pi)
   fit.alpha$varobj <- -fit.alpha$varobj
-  attr(fit.alpha, "class") <- "list"
+  fit.alpha$sa2    <- fit.alpha$data$sa2
+  fit.alpha$data   <- NULL
+  # attr(fit.alpha, "class") <- "list"
   
   # fitting mr.ash
   capture.output(fit <- mr_ash(data$X, data$y))
@@ -74,16 +55,39 @@ test_that("agreement with mr.ash.alpha", {
   fit$iter     <- max(fit$progress$iter)
   fit$progress <- NULL
   fit          <- fit[names(fit.alpha)]
-  attr(fit, "class") <- "list"
+  # attr(fit, "class") <- "list"
   
   # Skipping sa2 because of ongoing debate in slack channel
-  fit$data$sa2       <- NULL
-  fit.alpha$data$sa2 <- NULL
+  #fit$data$sa2       <- NULL
+  #fit.alpha$data$sa2 <- NULL
   
   # Check equivalence
-  expect_equal(fit, fit.alpha, scale = 1,tolerance = 1e-8)
+  expect_equal(fit, fit.alpha, scale = 1,tolerance = 1e-8, 
+               check.attributes = F, use.names = F)
 })
 
+
+# Test that mr.ash outputs the same phi, m, and s2 values as mr.ash.alpha
+test_that("check identical posterior", {
+  # Simulate X and y
+  set.seed(1)
+  n     <- 200
+  p     <- 400
+  pve   <- 0.2
+  s     <- 10
+  data  <- simulate_data(n, p, pve, s)
+  
+  # fitting mr.ash.alpha
+  capture.output(fit.alpha <- mr.ash.alpha::mr.ash(data$X, data$y))
+  post.alpha <- mr.ash.alpha::get.full.posterior(fit.alpha)
+  
+  # fitting mr.ash
+  capture.output(fit <- mr_ash(data$X, data$y))
+  post <- list(phi = fit$phi, m = fit$m, s2 = fit$s2)
+  
+  # Check same phi value
+  expect_equal(post, post.alpha, scale = 1,tolerance = 1e-8)
+})
 
 # ELBO output non-decreasing
 test_that("non-decreasing ELBO", {
