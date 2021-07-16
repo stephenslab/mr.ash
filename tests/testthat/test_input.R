2e809ee4121d307dfe9cadc0b9a2e4a6db679678
lwa19 <lwang19@uchicago.edu>
2021-07-16 12:13:27 -0500

    reorganized tests; added tests for lfsr

--- tests/testthat/test_input.R
+++ tests/testthat/test_input.R
@@ -1,3 +1,6 @@
+# Tests here check that the flags used in function mr_ash are functional,
+# and that function is not sensitive to X scaling. 
+
 context("input")
 
 # Testing that multiplying X by a constant does not affect output phi
@@ -12,7 +15,7 @@ test_that("Scaling X result in same phi", {
   epstol   <- 1e-12
   X.scaled <- data$X *  runif(1, -10, 10) + epstol
   
-  # fit mr.ash (X, y), (X.scaled, y), (X, y.scaled), (X.scaled, y.scaled)
+  # fit mr.ash (X, y), (X.scaled, y)
   capture.output(fit.Xy   <- mr_ash(data$X, data$y))
   capture.output(fit.Xsy  <- mr_ash(X.scaled, data$y))
 
@@ -36,10 +39,30 @@ test_that("Standardized X yield the same result regardless of
   X.std    <- scale(data$X, center = TRUE, scale = TRUE)
   
   # fit mr.ash (X, y), (X.std, y)
-  capture.output(fit.Xsy <- mr_ash_test(X.std, data$y, standardize = T))
-  capture.output(fit.Xy <- mr_ash_test(X.std, data$y))
+  capture.output(fit.Xsy <- mr_ash(X.std, data$y, standardize = T))
+  capture.output(fit.Xy  <- mr_ash(X.std, data$y))
   
   # Check that phi values are invariant wrt standardize flag
   expect_equal(fit.Xy$phi,fit.Xsy$phi,scale = 1,tolerance = 1e-8)
   # range(abs(fit.Xy$phi - fit.Xsy$phi))
+})
+
+
+# Check intercept flag: if the intercept flag is FALSE, fit$intercept == 0
+# Although this test is kind of useless once we know function doesn't throw
+# an error... 
+test_that("Check length of beta w/ and w/o intercept flat", {
+  
+  # Simulate X and y
+  set.seed(1)
+  n        <- 200
+  p        <- 400
+  pve      <- 0.2
+  s        <- 10
+  data     <- simulate_data(n, p, pve, s)
+  
+  # fit mr.ash (X, y), intercept == FALSE
+  capture.output(fit.Xy <- mr_ash(data$X, data$y, intercept = F))
+  expect_equal(fit.Xy$intercept, 0)
+  
 })
