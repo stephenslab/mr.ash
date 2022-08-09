78646c01811d88ea888d27eb3ac1c6c74f1b38c1
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-08-09 11:01:57 -0500

    A few minor fixes to simulate_regression_data.

--- R/datasim.R
+++ R/datasim.R
@@ -1,36 +1,36 @@
 #' @title Simulate Data from Multiple Regression Model
 #'
-#' @description Simulate regression data from a multiple regression
-#'   model.
+#' @description Simulate data from a multiple regression model.
 #'
-#' @details Data are generated from the model
-#' \deqn{y \sim N(b_0 + X b + Zu, \sigma^2 I),} in
-#' which the non-zero regression coefficients are drawn from the
-#' standard normal, and then scaled to achieve the desired proportion
-#' of variance in y explained by X (see input argument \code{pve}).
+#' @details Data are generated from the linear regressionmodel \deqn{y
+#' \sim N(b_0 + Xb + Zu, \sigma^2I),} in which the non-zero elements
+#' of vector \eqn{b} are drawn from the standard normal, and scaled to
+#' achieve the desired proportion of variance in y explained by X (see
+#' input argument \dQuote{pve}).
 #'
-#' @param n number of samples. Must be at least 2.
+#' @param n The number of samples. Should be 2 or greater.
 #'
-#' @param p number of regression variables. Must be at least 2.
+#' @param p The number of regression variables. Should be 2 or greater.
 #'
-#' @param s number of non-zero regression coefficients.
+#' @param s A non-negative number specifying the number of non-zero
+#'   regression coefficients.
 #'
-#' @param sigma standard deviation of the residual.
+#' @param sigma The standard deviation of the residual.
 #'
-#' @param pve proportion of variance in Y explained by X.
+#' @param pve The proportion of variance in y explained by X.
 #'
 #' @param center_X If \code{center_X = TRUE}, the columns of X are
 #'   centered before simulating y so that each column as a mean of
 #'   zero. Note that if \code{standardize_X = TRUE} this argument must
-#'   be set to \code{TRUE}.
+#'   also be set to \code{TRUE}.
 #'
 #' @param standardize_X If \code{standardize_X = TRUE}, the columns of
-#'   X are standardized before simulating y so that each column as a
+#'   X are standardized before simulating y so that each column has a
 #'   mean of zero and a standard deviation of 1.
 #'
-#' @param intercept intercept of regression equation, \eqn{b_0}.
+#' @param intercept The intercept \eqn{b_0} in the regression model.
 #'
-#' @param ncov (optional) number of covariates in Z. Defaults to 0
+#' @param ncov The number of covariates (columns of Z).
 #'
 #' @return A list with the following components:
 #'
@@ -73,10 +73,9 @@ simulate_regression_data <- function (
   center_X = TRUE,
   standardize_X = FALSE,
   intercept = 0,
-  ncov = 0
-) {
+  ncov = 0) {
 
-  # Argument checking
+  # Argument checking.
   if (!(is.scalar(n) && n >= 2))
     stop("Input argument \"n\" should be an integer equal to 2 or more")
   if (!(is.scalar(p) && p >= 2))
@@ -94,62 +93,55 @@ simulate_regression_data <- function (
   if (!(is.scalar(ncov) && ncov >= 0))
     stop("Input argument \"ncov\" should be an greater than or equal to 0 ",
          "(inclusive)")
-  if(!center_X && standardize_X)
-    stop("Input argument \"center_X\" must be set to TRUE if \"standardize_X\"",
-         " is TRUE")
-  if(pve == 1 && sigma != 0)
-    stop("If input argument \"pve\" is 1, \"sigma\" must be 0")
+  if (!center_X && standardize_X)
+    stop("Input argument \"center_X\" must be set to TRUE when ",
+         "\"standardize_X\" = TRUE")
+  if (pve == 1 && sigma != 0)
+    stop("If \"pve\" = 1, \"sigma\" must be 0")
 
-  # take floor of all integer arguments in case float was given
-  n <- floor(n)
-  p <- floor(p)
-  s <- floor(s)
+  # Take floor of all integer arguments (in case a non-integer value
+  # was given).
+  n    <- floor(n)
+  p    <- floor(p)
+  s    <- floor(s)
   ncov <- floor(ncov)
 
-  # simulate data matrix X:
-  X <- matrix(rnorm(n * p), n, p)
-  X <- scale(X, center = center_X, scale = standardize_X)
+  # Simulate data matrix.
+  X <- matrix(rnorm(n*p), n, p)
+  X <- scale(X,center = center_X,scale = standardize_X)
 
-  # check if s <= p
+  # Check if s <= p.
   if (s > p) {
     stop("number of effects more than variables")
   }
 
-  # generate effect variables
-  beta.idx = sample(p, s)
-  beta = rep(0, p)
+  # Generate the regression coefficientss.
+  beta.idx <- sample(p, s)
+  beta <- rep(0, p)
 
   # generate effects
   if(s > 0){
+    beta.values <- rnorm(s)
+    beta[beta.idx] <- beta.values
 
-    beta.values = rnorm(s)
-    beta[beta.idx] = beta.values
-
-    # Adjust the effects so that we control for the proportion of variance
-    # explained (pve). That is, we adjust beta so that r = a/(a+1), where we
-    # define a = beta'*cov(X)*beta. Here, sb is the variance of the (nonzero)
-    # effects.
+    # Adjust the effects so that we control for the proportion of
+    # variance explained (pve). That is, we adjust beta so that r =
+    # a/(a+1), where we define a = beta'*cov(X)*beta. Here, sb is the
+    # variance of the (nonzero) effects.
     if (pve < 1) {
-
       sb   <- pve/(1-pve)/var(drop(X %*% beta))
       beta <- sqrt(sb * sigma) * beta
-
     }
-
   }
 
   # Generate the covariate data (Z), and the linear effects of the
   # covariates (u).
   if (ncov > 0) {
-    Z <- matrix(
-      data = rnorm(n * ncov),
-      nrow = n,
-      ncol = ncov
-    )
+    Z <- matrix(rnorm(n*ncov),n,ncov)
     u <- rnorm(ncov)
   } else {
-    Z <- NULL
-    u <- NULL
+    Z <- as.numeric(NA)
+    u <- as.numeric(NA)
   }
 
   # Generate the quantitative trait measurements.
@@ -158,25 +150,21 @@ simulate_regression_data <- function (
     y <- y + Z %*% u
   y <- drop(y)
 
-  # give names to outputs
-  rnames <- paste0("s", 1:n)
-  cnames_X <- paste0("v", 1:p)
-  names(y) <- rnames
+  # Add row and column names to the outputs.
+  rnames      <- paste0("s", 1:n)
+  cnames_X    <- paste0("v", 1:p)
+  names(y)    <- rnames
   rownames(X) <- rnames
   colnames(X) <- cnames_X
   names(beta) <- cnames_X
-
   if (ncov > 0) {
-
-    cnames_Z <- paste0("c", 1:ncov)
-    names(u) <- cnames_Z
+    cnames_Z    <- paste0("c", 1:ncov)
+    names(u)    <- cnames_Z
     colnames(Z) <- cnames_Z
     rownames(Z) <- rnames
-
   }
 
-  return(
-    list(
+  return(list(
       y = y, X = X, Z = Z, u = u, beta = beta, intercept = intercept,
       sigma = sigma
     )
