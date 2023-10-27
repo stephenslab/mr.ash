08f3ba2be903b0f104d3f418f80d9a83a244dec0
Matthew Stephens <stephens999@gmail.com>
2023-10-27 17:17:27 -0500

    add tests for scaling

--- tests/testthat/test_fit.R
+++ tests/testthat/test_fit.R
@@ -65,3 +65,30 @@ test_that("lfsr between 0 and 1", {
   test <- sum(fit$lfsr < 0 | fit$lfsr > 1)
   expect_equal(test, 0, scale = 1, tolerance = 1e-8)
 })
+
+
+# lfsr calculation should yield output between 0 and 1
+test_that("computations scale appropriately with X and y", {
+  # Simulate X and y
+  set.seed(2)
+  n     <- 200
+  p     <- 400
+  pve   <- 0.2
+  s     <- 10
+  data  <- simulate_regression_data(n = n, p = p, pve = pve, s = s)
+
+  # fit mr.ash
+  capture.output(fit <- fit_mr_ash(data$X, data$y))
+  capture.output(fit2 <- fit_mr_ash(data$X*10, data$y*2))
+
+  expect_equal(fit2$b/fit$b, rep(2/10,p)) # beta scales with y/X
+  expect_equal(fit2$resid.var/fit$resid.var, 2^2) # scales with y^2
+  expect_equal(fit2$prior$var/fit$prior$var,0.01) # with 1/X^2
+
+  expect_equal(fit2$phi/fit$phi, 1) # this is very idealized! Probably won't happen
+  expect_equal(fit2$m/fit$m, 2/10) # m scales with b
+  expect_equal(fit2$s2/fit$s2, (2/10)^2) # scales with b^2
+
+})
+
+
